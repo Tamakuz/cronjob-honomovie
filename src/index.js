18494d4c2869060d4b1429246c6c6bfe0a44ff9f
Tamakuz <jovangithub@gmail.com>
2024-09-15 00:03:19 +0700

    Update GitHub Actions configuration to run job every minute

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import { fetchData } from "./fetchData.js";
 
+// Jalankan fetchData setiap 1 menit (60000 ms)
+setInterval(fetchData, 60000);
+
+// Jalankan fetchData segera setelah script dijalankan
 fetchData();
